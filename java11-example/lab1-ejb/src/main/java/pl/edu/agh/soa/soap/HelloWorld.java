2a6c496d38fd59cbbbcd64eaa70b744394c2a460
Marek <marek2105@vp.pl>
2020-09-19 01:14:17 +0200

    Added big exam project

---
+++ java11-example/lab1-ejb/src/main/java/pl/edu/agh/soa/soap/HelloWorld.java
@@ -0,0 +1,26 @@
+package pl.edu.agh.soa.soap;
+
+//import org.jboss.annotation.security.SecurityDomain;
+//import org.jboss.ws.api.annotation.WebContext;
+
+import javax.ejb.Stateless;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+
+@Stateless
+@WebService
+//@WebContext(contextRoot="/soa", urlPattern="/hello")
+
+@SOAPBinding(style = SOAPBinding.Style.RPC, use = SOAPBinding.Use.LITERAL)
+public class HelloWorld {
+
+    @WebMethod(action="hello")
+    @WebResult(name="result")
+    public String Hi(@WebParam(name = "name") String name) {
+        return "Hello " + name;
+    }
+
+}
